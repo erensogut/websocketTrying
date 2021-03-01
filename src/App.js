32f0533dad538e0399087c49ee740abd7c7f3e12
Eren <erensogut1905@gmail.com>
2021-03-01 20:47:08 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+
 
 function App() {
+  const connection = new WebSocket('wss://fstream.binance.com/ws/btcusdt@bookTicker');
+  const [curr, setCurr] = useState();
+  //const array = useRef([]);
+  const array = [];
+  useEffect(() => {
+    //var connection = new WebSocket('wss://fstream.binance.com/ws/btcusdt@bookTicker');
+    // listen to onmessage event
+    connection.onmessage = evt => {
+      // add the new message to state
+
+      //messages: this.state.messages.concat([evt.data])
+
+      var parsedJSON = JSON.parse(evt.data);
+      //array.push(parsedJSON.a);
+      //console.log(array);
+      //localStorage.setItem('ereb', '"');
+      //localStorage.setItem('myValueInLocalStorage', array);
+      setCurr(parsedJSON.a);
+    }
+  }, curr); // See Note 2
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Now, the most current Bitcoin Usd coin is
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{curr}</p>
+
       </header>
     </div>
   );
